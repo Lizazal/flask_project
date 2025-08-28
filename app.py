f5f119daa1f17343aba17c7c60ccb3adb1b277f5
Lizazal <liza.rzal@gmail.com>
2025-08-28 17:07:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,41 @@ app = Flask(__name__)
 
 # Определяем маршрут и привязываем его к функции
 @app.route('/')
-def hello():
+def hello_flask():
     return "Hello, Flask!"
+
+
+@app.route('/hello')
+def hello():
+    return "Hello, world!"
+
+
+@app.route('/info')
+def info():
+    return "This is an informational page."
+
+
+@app.route('/calc/<a>/<b>')
+def calc(a, b):
+    try:
+        return f"The sum of {a} and {b} is {int(a)+int(b)}."
+    except ValueError:
+        return "I can't calculate this one."
+
+
+@app.route('/reverse/<text>')
+def reverse(text):
+    return f"{text[::-1]}"
+
+
+@app.route('/reverse/')
+def no_reverse():
+    return "There's nothing to reverse"
+
+
+@app.route('/user/<string:name>/<int:age>')
+def user(name, age):
+    return f"Hello, {name}. You are {age} years old."
 
 
 # Запуск приложения
