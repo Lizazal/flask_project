c190120e687872f5021f254fec57417ccab6d2df
Lizazal <liza.rzal@gmail.com>
2025-09-17 12:41:27 +0300

    /form (+form.html) and /submit (+result.html) added

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
 
 @app.route('/')
@@ -43,3 +43,18 @@ def no_reverse():
 @app.route('/user/<string:name>/<int:age>')
 def user(name, age):
     return f"Hello, {name}. You are {age} years old."
+
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
+
+@app.route("/submit", methods=["POST", "GET"])
+def submit():
+    if request.method == "POST":
+        name = request.form.get("name")  # Получаем имя из формы
+        email = request.form.get("email")  # Получаем email из формы
+        return render_template("result.html", name=name, email=email)
+    else:
+        return redirect(url_for("form"))  # Если запрос GET, возвращаем на форму
